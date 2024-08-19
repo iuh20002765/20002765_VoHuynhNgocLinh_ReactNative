6fcb071ae59e68879887f104a69caafbf08e9d54
NgocLinh3110 <linhvo31102002@gmail.com>
2024-08-19 17:05:17 +0700

    Challenge06.js + Minh chung

---
+++ Week1/Code/Challenge06.js
@@ -0,0 +1,22 @@
+//Bills
+const bills = [125, 555, 44];
+
+//Tips
+const tips = bills.map(calcTip);
+
+//Array
+const total = bills.map((bill, index) => bill + tips[index]);
+
+//calTips
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15; // 15% tip
+  } else {
+    return bill * 0.2; // 20% tip
+  }
+}
+
+//Resolves the problem
+console.log("Bills:", bills);
+console.log("Tips:", tips);
+console.log("Total:", total);
