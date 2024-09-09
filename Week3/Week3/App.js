919c1aded2cf65e2945e2c5789d5166665c56df6
NgocLinh3110 <linhvo31102002@gmail.com>
2024-09-09 17:00:35 +0700

    Week3 FirstScreen + MinhChung fix

--- Week3/Week3/App.js
+++ Week3/Week3/App.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
   buttonView: {
     marginTop: 50,
     flexDirection: "row",
-    justifyContent: "space-around",
+    gap: 20,
   },
   button: {
     alignItems: "center",
